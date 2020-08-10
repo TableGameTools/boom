834957c4b185df50f764d6cadf835a0d6e2cf65a
TableGameTools <69444658+TableGameTools@users.noreply.github.com>
2020-08-10 11:18:36 +0800

    Add files via upload

---
+++ precache-manifest.e9b66a578a40d331074c6009f73ce00e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bb8e913fb97749e89a225c5691254a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b8f74292d90b75187e25",
+    "url": "./static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "73643f616bccea6fc97a",
+    "url": "./static/css/main.867e7cfd.chunk.css"
+  },
+  {
+    "revision": "b8f74292d90b75187e25",
+    "url": "./static/js/2.18b7c132.chunk.js"
+  },
+  {
+    "revision": "f78c1a232f52b811f0606e3ef48056c3",
+    "url": "./static/js/2.18b7c132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73643f616bccea6fc97a",
+    "url": "./static/js/main.d2e978df.chunk.js"
+  },
+  {
+    "revision": "23bc77cca0be7ca5cd32",
+    "url": "./static/js/runtime-main.c2ac8707.js"
+  }
+]);
